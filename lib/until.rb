2614c933e9f50444e66b89d2ba14a58303111334
Makeda P Mckune <makedamckune@gmail.com>
2019-10-09 14:45:03 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,8 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
+  counter = 6 
+  until = counter == 10 
+  puts " The current levation force is less than 10"
+  counter += 4
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,9 @@
 def using_while
   levitation_force = 6
+	while counter < 10 
+	puts "The levitation force is less than 10"
+	counter += 4 
 	
-  #your code here
 end
 
 
